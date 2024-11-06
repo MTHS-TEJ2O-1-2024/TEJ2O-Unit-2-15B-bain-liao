107ba5d43e6a2da6e90bad6b8092d6b08fc1b63f
bain-liao <liao.bain@mths.ca>
2024-11-05 19:43:30 -0500

    created program that moves a pixel around the edge using nested loops

--- main.ts
+++ main.ts
@@ -8,40 +8,30 @@
 // variables
 let sprite: game.LedSprite = null
 let loopCounter = 0
+let steps = 0
 
 // setup
 basic.showIcon(IconNames.Happy)
 
-// when "A" is pressed, the pixels move clockwise around the outside edge of the LED matrix
+// when "A" is pressed, the pixels move around the outside edge of the LED matrix
 input.onButtonPressed(Button.A, function () {
     // setup
     basic.clearScreen()
     loopCounter = 0
     sprite = game.createSprite(0, 0)
-    // the pixel moves around the perimeter clockwise
-    while (loopCounter <= 5) {
-        sprite.set(LedSpriteProperty.X, loopCounter)
-        loopCounter = loopCounter + 1
-        basic.pause(500)
+    basic.pause(500)
+    // Loop to move the pixel around the outside edge
+    while (loopCounter < 4) {
+        steps = 0
+        while (steps < 4) {
+            sprite.move(1)
+            basic.pause(500)
+            steps ++
+        }
+        sprite.turn(Direction.Right, 90)
+        loopCounter ++
     }
-    loopCounter = 0
-    while (loopCounter <= 5) {
-        sprite.set(LedSpriteProperty.Y, loopCounter)
-        loopCounter = loopCounter + 1
-        basic.pause(500)
-    }
-    loopCounter = 5
-    while (loopCounter >= 0) {
-        sprite.set(LedSpriteProperty.X, loopCounter)
-        loopCounter = loopCounter - 1
-        basic.pause(500)
-    }
-    loopCounter = 5
-    while (loopCounter >= 0) {
-        sprite.set(LedSpriteProperty.Y, loopCounter)
-        loopCounter = loopCounter - 1
-        basic.pause(500)
-    }
+    // Delete sprite and show a happy icon when done
     sprite.delete()
     basic.showIcon(IconNames.Happy)
 })
